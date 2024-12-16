d1e63d8db9e7bd4cea17e09f0d15f4bcc76b83af
kiu_shubitidze.sandro <shubitidze.sandro@kiu.edu.ge>
2024-12-16 22:16:06 +0400

    Add vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme.ts'
+import { Analytics } from "@vercel/analytics/react"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics/>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
